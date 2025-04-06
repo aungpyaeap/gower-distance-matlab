f2e3565423f16e6064d5ceb1519c0510ccd055d3
Aung Pyae <aungpyaemcc@gmail.com>
2025-04-06 11:46:23 +0800

    update sample

--- sample.m
+++ sample.m
@@ -1,9 +1,18 @@
-clc; clear; close all;
-rng('default');
+clc;
+clear;
+close all;
 
-load fisheriris.mat;
+% Define data
+Gender = {'male'; 'female'; 'male'; 'female'; 'male'; 'male'; 'female'; 'male'};
+Age = [19; 23; 36; 24; 32; 26; 22; 25];
+Height = [176; 166; 185; 175; 194; 182; 170; 174];
+Hobby = {'music'; 'writing'; 'football'; 'tennis'; 'basketball'; 'tennis'; 'writing'; 'music'};
 
-dataPoints = array2table(meas);
+dataPoints = table(Gender, Age, Height, Hobby);
+
+fprintf('Dataset %d x %d matrix.\n', height(dataPoints), width(dataPoints));
+disp(dataPoints);
 
 DIST = GowerDistance(dataPoints);
+fprintf('Gower distance matrix %d x %d matrix.\n', height(DIST), width(DIST));
 disp(DIST);
